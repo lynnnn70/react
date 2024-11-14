3f3ce228f3cb4b3b4639a1cdf63c9c8aa97a3936
lynnnn70 <lynnchiang1220@gmail.com>
2024-11-14 17:49:12 +0800

    add annotation

--- src/fruits.jsx
+++ src/fruits.jsx
@@ -4,6 +4,10 @@
 const Fruit = (props) => {   //Fruit 為component
     return <p> this is {props.fruit_name}.</p>   //<p>裡面的為element
 }
+// Fruit 是一個 React 元件（函數元件）
+// 在 React 中，props 是一種用來讓元件之間進行資料溝通的方式，特別是從父元件向子元件傳遞資料。
+// props 是一個物件
+// {props.fruit_name} 是從 props 物件中取得的屬性，並顯示在 <p> 標籤中。
 
 //類別宣告
 // class Fruit extends React.Component {
--- src/index.js
+++ src/index.js
@@ -5,19 +5,21 @@ import App from './App';
 import FruitApp from './FruitApp';
 import reportWebVitals from './reportWebVitals';
 
-const appRoot = ReactDOM.createRoot(document.getElementById('app-root'));
-appRoot.render(
+// React.StrictMode: 開發模式包裝器，幫助開發者檢測潛在問題
+// EX:過時的 API 使用、非最佳實踐等，在生產環境中不會有任何影響。
+//不需要在每個元件中都使用 <React.StrictMode>。
+//通常只需在應用程式的根層級（entry point）使用一次即可，
+//這樣它就會自動套用到應用程式內的所有子元件中。
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
-
-const fruitRoot = ReactDOM.createRoot(document.getElementById('fruit-root'));
-fruitRoot.render(
-  <React.StrictMode>
     <FruitApp />
   </React.StrictMode>
-)
+);
+// <App />：渲染 App 元件至 app-root 節點
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
